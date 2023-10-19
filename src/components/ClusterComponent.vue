43cc5a388a9d80c9d8f12570159127abdea75231
iggy <iggynelo@gmail.com>
2023-10-19 18:43:12 +0900

    created cluster component

---
+++ src/components/ClusterComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="item item1">1</div>
+    <div class="item">2</div>
+    <div class="item">3</div>
+  </div>
+</template>
+<style scoped>
+.container {
+  height: 500px;
+  width: 800px;
+  border: 3px solid red;
+  display: flex;
+}
+.item {
+  border: 5px solid green;
+  background-color: lightblue;
+  color: black;
+  font-size: 50px;
+  text-align: center;
+}
+
+/* .item1 {
+  flex-grow: 1;
+} */
+</style>
